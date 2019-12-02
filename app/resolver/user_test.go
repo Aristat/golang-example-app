15374ce99b47cc73b89fd45f3d353a2b6243d094
Alexander Strelnikov <aristat37@gmail.com>
2019-12-02 22:42:50 +0300

    Updated user tests

--- app/resolver/user_test.go
+++ app/resolver/user_test.go
@@ -15,6 +15,12 @@ import (
 	mocket "github.com/selvatico/go-mocket"
 )
 
+type UserParams struct {
+	testName string
+	id       int
+	email    string
+}
+
 func TestOne(t *testing.T) {
 	ctx := context.Background()
 
@@ -36,34 +42,36 @@ func TestOne(t *testing.T) {
 	}
 
 	tests := []struct {
-		name    string
-		id      int
-		email   string
-		mock    func()
-		asserts func(err error)
+		userParams UserParams
+		mock       func()
+		asserts    func(userParams UserParams, out *graphql1.UsersOneOut, err error)
 	}{
 		{
-			name:  "USER EXIST",
-			id:    id,
-			email: defaultEmail,
-			mock:  mockDefaultResult,
-			asserts: func(err error) {
+			userParams: UserParams{id: 2, email: "test1@gmail.com", testName: "USER NOT EXIST"},
+			mock:       mockDefaultResult,
+			asserts: func(userParams UserParams, out *graphql1.UsersOneOut, err error) {
+				assert.NotNil(t, err, "err should not be nil")
+				assert.Nil(t, out, "out should be nil")
+			},
+		},
+		{
+			userParams: UserParams{id: id, email: defaultEmail, testName: "USER EXIST"},
+			mock:       mockDefaultResult,
+			asserts: func(userParams UserParams, out *graphql1.UsersOneOut, err error) {
 				assert.Nil(t, err, "err should be nil")
+				assert.Equal(t, cast.ToString(userParams.id), out.ID, "id should be equals")
+				assert.Equal(t, userParams.email, out.Email, "email should be equals")
 			},
 		},
 	}
 
 	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
+		t.Run(test.userParams.testName, func(t *testing.T) {
 			mocket.Catcher.Reset()
 			test.mock()
 
-			out, _ := cfg.Resolvers.UsersQuery().One(ctx, &obj, "test@gmail.com")
-			t.Log(out.ID, out.Email)
-
-			assert.Equal(t, cast.ToString(test.id), out.ID, "id should be equals")
-			assert.Equal(t, test.email, out.Email, "email should be equals")
-			test.asserts(err)
+			out, err := cfg.Resolvers.UsersQuery().One(ctx, &obj, test.userParams.email)
+			test.asserts(test.userParams, out, err)
 		})
 	}
 }
